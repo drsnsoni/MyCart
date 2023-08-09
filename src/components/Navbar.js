99983475e98a914eedc4558beb6fe21357c4ab09
drsnsoni <100061941+drsnsoni@users.noreply.github.com>
2023-08-09 10:31:15 +0530

    Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
 class Navbar extends React.Component {
-  state = {};
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            My Cart
+            Apna Cart
           </a>
         </div>
       </nav>
